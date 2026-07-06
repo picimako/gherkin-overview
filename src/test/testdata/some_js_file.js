203afabec83ccd03b7f4fb4fc7b82acb6f985132
Tamás Balog <picimako@protonmail.com>
2026-07-06 11:18:52 +0200

    Replace year in copyright

--- src/test/testdata/some_js_file.js
+++ src/test/testdata/some_js_file.js
@@ -1,4 +1,2 @@
-/*
- * Copyright 2022 Tamás Balog. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
- */
+//Copyright 2026 Tamás Balog. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 
